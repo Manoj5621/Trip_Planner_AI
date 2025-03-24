61b16568d186712013dd95e27fbcd942fce0bc0e
Anika Tiwari <tiwarianika42@gmail.com>
2025-03-24 14:51:46 +0530

    Frontend Changes

--- trip_planner/settings.py
+++ trip_planner/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',  # Required for allauth
+    'django.contrib.sites',  
     
     # Local apps
     'trips.apps.TripsConfig',
