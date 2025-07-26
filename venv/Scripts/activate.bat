6b7fe208b21d84e28717909b2e411ee75cca817a
Sanaapathann <sanashraf2725@gmail.com>
2025-07-26 09:49:00 +0530

    static files updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\abhis\OneDrive\Desktop\Shazmeen\AI_Trip_Planner_2\venv
+set VIRTUAL_ENV=C:\Users\Sana Pathan\OneDrive\Desktop\sana\AITrip_planner\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
